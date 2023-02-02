73af12387452aa2ed90783b746d482f07ba334a6
unknown <freelancestanislav@gmail.com>
2023-02-03 00:39:13 +0300

    fix styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Controller } from "./component/controller/controller";
@@ -6,11 +6,67 @@ import { Display } from "./component/display/display";
 import { Home } from "./component/home/home";
 
 function App() {
+  async function setStyles() {
+    document.head.childNodes.forEach(async (el: any) => {
+      if (el.nodeName === "LINK") {
+        if (el.href.includes(".css")) {
+          let url = "";
+
+          el.href
+            .split("//")[1]
+            .split("/")
+            .forEach((str: string, index: number) => {
+              if (index !== 0) {
+                url += `/${str}`;
+              }
+            });
+
+          const cssFile = await fetch(url)
+            .then((response) => response.body)
+            .then((rb: any) => {
+              const reader = rb.getReader();
+
+              return new ReadableStream({
+                start(controller) {
+                  function push() {
+                    reader.read().then(({ done, value }: any) => {
+                      if (done) {
+                        controller.close();
+                        return;
+                      }
+                      controller.enqueue(value);
+                      push();
+                    });
+                  }
+                  push();
+                },
+              });
+            })
+            .then((stream) =>
+              new Response(stream, {
+                headers: { "Content-Type": "text/html" },
+              }).text()
+            )
+            .then((result) => {
+              return result;
+            });
+
+          const styleElem = document.createElement("style")
+          styleElem.innerHTML = cssFile
+
+          document.body.appendChild(styleElem)
+        }
+      }
+    });
+  }
+
+  useEffect(() => {
+    setStyles();
+  }, []);
+
   return (
     <Router>
-      <div className="App">
-        
-      </div>
+      <div className="App"></div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/display" element={<Display />}></Route>
