127391470d89ca6f79c32361382c2aea7f8ebb0f
unknown <8268363@gmail.com>
2023-06-28 00:19:28 +0300

    redesign toast

--- client/src/app/components/notfound/notfound.component.ts
+++ client/src/app/components/notfound/notfound.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,12 +6,18 @@ import { Router } from '@angular/router';
   templateUrl: './notfound.component.html',
   styleUrls: ['./notfound.component.scss']
 })
-export class NotfoundComponent {
+export class NotfoundComponent implements OnInit {
   public url: string = window.location.pathname
 
   constructor(
     private router: Router
   ) {}
+
+  ngOnInit(): void {
+    if (window.location.pathname.length > 10) {
+      this.url = window.location.pathname.slice(0, 10) + '...'
+    }
+  }
 
   public goHome(): void {
     this.router.navigate(['/home'])
