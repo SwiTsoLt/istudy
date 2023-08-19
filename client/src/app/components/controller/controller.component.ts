6de87726c1ce85c2db78a777f2bb6999872612e0
unknown <8268363@gmail.com>
2023-08-19 23:15:40 +0300

    add start controller

--- client/src/app/components/controller/controller.component.ts
+++ client/src/app/components/controller/controller.component.ts
@@ -20,41 +20,18 @@ export class ControllerComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.position.x = 180 / 180 * 50 + "%"
+    this.position.y = 180 / 180 * 50 + "%"
+
     this.subscribeToDeviceOrientation()
   }
 
   private subscribeToDeviceOrientation(): void {
     window.addEventListener("deviceorientation", event => {
       if (event.alpha !== null && event.beta !== null) {
-        this.position.x = this.calculateAlpha(event.alpha, event.beta)
-        this.position.y = this.calculateBetta(event.alpha, event.beta)
+        this.position.x = event.alpha / 180 * 50 + "%"
+        this.position.y = event.beta / 180 * 50 + "%"
       }
     })
-  }
-
-  private calculateAlpha(alpha: number, betta: number): string {
-    const normalizePos1 = alpha / 180 * 50 // like a radius
-    const normalizePos2 = betta / 180 * 50
-
-    const diametr = normalizePos2 * 2
-
-    const result = Math.sqrt(Math.abs(diametr - normalizePos1**2))
-
-    console.log(result);
-
-    return `calc(${result}%)`
-  }
-
-  private calculateBetta(alpha: number, betta: number): string {
-    const normalizePos1 = alpha / 180 * 50
-    const normalizePos2 = betta / 180 * 50
-
-    const diametr = normalizePos1 * 2
-
-    const result = Math.sqrt(Math.abs(diametr - normalizePos2**2))
-
-    console.log(result);
-
-    return `calc(${result}%)`
   }
 }
