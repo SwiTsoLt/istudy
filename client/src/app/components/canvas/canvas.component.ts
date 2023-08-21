9daddc1efee751c535342410d72106fb899a385c
unknown <8268363@gmail.com>
2023-08-21 19:29:26 +0300

    update invirsy by y

--- client/src/app/components/canvas/canvas.component.ts
+++ client/src/app/components/canvas/canvas.component.ts
@@ -17,8 +17,8 @@ export class CanvasComponent implements OnInit, AfterViewInit {
   private cameraPosition$: Observable<IPosition> = this.canvasStore$.pipe(select(canvasSelectors.selectCameraPosition))
 
   public sensitivity: number = 0.02
-  public inverseX: number = 1
-  public inverseY: number = -1
+  public inverseX: number = -1
+  public inverseY: number = 1
 
   constructor(
     private canvasStore$: Store<CanvasState>
