4c4b193ce67eea8e3c2343eafd5cd44f63681608
unknown <8268363@gmail.com>
2023-11-25 23:34:16 +0300

    clear folders and code

--- client/src/app/components/pages/canvas/maps/math/boat.ts
+++ client/src/app/components/pages/canvas/maps/math/boat.ts
@@ -40,35 +40,12 @@ export const map: IMapData = {
         }
     },
     "scene": [
-        // {
-        //     ...EntityDefaults,
-        //     "title": "Boat",
-        //     "type":  entityTypeEnum.model,
-        //     "model": "boat/scene.gltf",
-        //     "credit": "This work is based on \"Fishing boat\" (https://sketchfab.com/3d-models/fishing-boat-f4b38ccf5ffb46018aa7931c0e106654) by Pabooklas (https://sketchfab.com/Pabooklas) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)",
-        //     "multiplyScalar": 2,
-        //     "scale": {
-        //         "width": 1,
-        //         "height": 1,
-        //         "depth": 1,
-        //     },
-        //     "position": {
-        //         "x": 0,
-        //         "y": 0,
-        //         "z": -1
-        //     },
-        //     "rotation": {
-        //         "x": -90,
-        //         "y": 0,
-        //         "z": 180
-        //     }
-        // },
         {
             ...EntityDefaults,
             "title": "Boat",
             "type":  entityTypeEnum.model,
-            "model": "fboat/scene.gltf",
-            "credit": "This work is based on \"Fishing boat\" (https://sketchfab.com/3d-models/fishing-boat-f4b38ccf5ffb46018aa7931c0e106654) by Pabooklas (https://sketchfab.com/Pabooklas) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)",
+            "model": "boat/scene.gltf",
+            "credit": "This work is based on \"Fishing Boat\" (https://sketchfab.com/3d-models/fishing-boat-e07c8b9cc38543879a4e2fe145e62df6) by JasperTobias (https://sketchfab.com/JasperTobias) licensed under CC-BY-NC-4.0 (http://creativecommons.org/licenses/by-nc/4.0/)",
             "multiplyScalar": 0.015,
             "scale": {
                 "width": 1,
@@ -182,50 +159,5 @@ export const map: IMapData = {
                 "z": 0
             }
         },
-        // {
-        //     ...EntityDefaults,
-        //     "type": EntityTypeEnum.sphere,
-        //     "materialType": EntityMaterialTypeEnum.phong_shininess,
-        //     "color": 0x60EEFF,
-        //     "scale": {
-        //         "width": 0.1,
-        //         "height": 10,
-        //         "depth": 10,
-        //     },
-        //     "position": {
-        //         "x": 0,
-        //         "y": 0,
-        //         "z": -1
-        //     },
-        //     "rotation": {
-        //         "x": 30,
-        //         "y": 30,
-        //         "z": 0
-        //     }
-        // },
-        // {
-        //     ...EntityDefaults,
-        //     "title": "Water",
-        //     "type": entityTypeEnum.model,
-        //     "credit": "This work is based on \"Water Animation\" (https://sketchfab.com/3d-models/water-animation-e54ff76bef854b128af8d20cf9c03729) by Artise1",
-        //     "model": "water/scene.gltf",
-        //     "modelType": modelTypeEnum.gltf,
-        //     "scale": {
-        //         "width": 2,
-        //         "height": 2,
-        //         "depth": 2,
-        //     },
-        //     "multiplyScalar": 0.01,
-        //     "position": {
-        //         "x": 0,
-        //         "y": -3,
-        //         "z": 9
-        //     },
-        //     "rotation": {
-        //         "x": 90,
-        //         "y": 0,
-        //         "z": 0
-        //     }
-        // },
     ]
 };
